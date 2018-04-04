f5ce317a87a134c68eefd0299dbb9a69e25301af
David N Zilio <DavidZilio@cMail.Carleton.ca>
2018-04-04 01:37:25 -0400

    Changes to the server making the server respond to requests still not logging or actually doing any password work

---
+++ ReadMe.md
@@ -0,0 +1,28 @@
+# Fruity Authenticator
+
+Carleton University\
+COMP 3008-A\
+Winter 2018\
+R. Biddle
+
+Group 27\
+David N. Zilio\
+100997259
+
+Project was started\
+Monday April 2<sup>nd</sup> 2018
+
+## Assignment 2
+
+### Part 2 Instruction 2
+
+This is a NodeJS application developed on:
+
+* Firefox 59.0.2 (64-Bit)
+* Windows 10 Version 1709 Build 16299.334
+* NodeJS version 8.10.0
+* npm version 5.8.0
+
+install everything above run "npm install" then "npm test" and connect to http://localhost:8080/" on Firefox to access
+
+This application's purpose is to test a password scheme
--- Server.js
+++ Server.js
@@ -1,6 +1,37 @@
-const express = require('express')
-const app = express()
+const fs        = require('fs');
+const express   = require('express');
+const app       = express();
+const mime      = require('mime-types');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const PORT      = 8080;
+const rootDir   = "./files"
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use((req, res, next) => {console.log('HTTP request for\n    ' + req.url); next();})
+
+app.get('/', function (req, res) { 
+    filePath = rootDir + "/index.html";
+    fs.readFile(filePath, function(err, data) {
+        if (err)
+            res.send(404);
+        else {
+            res.writeHead(200, {'Content-Type': mime.lookup(filePath)});
+            res.write(data);
+            res.end();
+        }
+    });
+})
+
+app.get('/*', function (req, res) { 
+    filePath = rootDir + req.url;
+    fs.readFile(filePath, function(err, data) {
+        if (err)
+            res.send(404);
+        else {
+            res.writeHead(200, {'Content-Type': mime.lookup(filePath)});
+            res.write(data);
+            res.end();
+        }
+    });
+})
+
+app.listen(PORT, () => console.log('Example app listening on port ' + PORT + '!'))
